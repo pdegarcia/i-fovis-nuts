17d26437bde9619701a2b67ac0255b011e3edcf0
Paulo Garcia <paulo.garcia@tecnico.ulisboa.pt>
2015-11-18 18:06:07 +0000

    Added graphs.

--- js/explore.js
+++ js/explore.js
@@ -1,66 +1,141 @@
 !(function (d3) {
 
-$("acontent").empty();
+$("explorecontent").empty();
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+  width = 400 - margin.left - margin.right,
+  height = 220 - margin.top - margin.bottom;
 
+// Parse the date / time
 var parseDate = d3.time.format("%d-%b-%y").parse;
 
-var x = d3.time.scale()
-    .range([0, width]);
+// Set the ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+  .orient("bottom").ticks(5);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+var yAxis = d3.svg.axis().scale(y)
+  .orient("left").ticks(5);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.val); });
-
-var svg = d3.select("acontent").append("svg")
+// Define the line
+var valueline = d3.svg.line()
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.close); });
+    
+// Adds the svg canvas - PRIMEIRO GRAFICO - GANHO POR NIVEL DE ESCOLARIDADE
+var chart1 = d3.select("explorecontent")
+  .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data-a.tsv", function(error, data) {
+// Get the data
+d3.csv("data1.csv", function(error, data) {
   data.forEach(function(d) {
     d.date = parseDate(d.date);
-    d.val = +d.val;
+    d.close = +d.close;
   });
 
+  // Scale the range of the data
   x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.val; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  // Add the valueline path.
+  chart1.append("path")
+    .attr("class", "line")
+    .attr("d", valueline(data));
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Price A ($)");
+  // Add the X Axis
+  chart1.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
+  // Add the Y Axis
+  chart1.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
 
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
 });
+
+// Adds the svg canvas - SEGUNDO GRAFICO - PODER DE COMPRA PER CAPITA
+var chart2 = d3.select("explorecontent")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+// Get the data
+d3.csv("data1.csv", function(error, data) {
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.close = +d.close;
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+  // Add the valueline path.
+  chart2.append("path")
+    .attr("class", "line")
+    .attr("d", valueline(data));
+
+  // Add the X Axis
+  chart2.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  // Add the Y Axis
+  chart2.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+});
+
+// Adds the svg canvas - TERCEIRO GRAFICO - CONSULTAS POR HABITANTE
+var chart3 = d3.select("explorecontent")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+// Get the data
+d3.csv("data1.csv", function(error, data) {
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.close = +d.close;
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+  // Add the valueline path.
+  chart3.append("path")
+    .attr("class", "line")
+    .attr("d", valueline(data));
+
+  // Add the X Axis
+  chart3.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  // Add the Y Axis
+  chart3.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+});
+
 
 })(d3);
