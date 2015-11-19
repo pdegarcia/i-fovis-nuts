0c3309eb9744d3836adf9026412706e61284015e
dariornelas <dario.ornelas@tecnico.ulisboa.pt>
2015-11-19 18:51:57 +0000

    dropdown menu, inicio da leitura do nosso dataset. inclui um dataset-sample para testar é mais fácil.

--- js/compare.js
+++ js/compare.js
@@ -1,66 +1,88 @@
 !(function (d3) {
 
-$("bcontent").empty();
+$("comparecontent").empty();
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+  width = 800 - margin.left - margin.right,
+  height = 420 - margin.top - margin.bottom;
 
+// Parse the date / time
 var parseDate = d3.time.format("%d-%b-%y").parse;
 
-var x = d3.time.scale()
-    .range([0, width]);
+// Set the ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+  .orient("bottom").ticks(5);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+var yAxis = d3.svg.axis().scale(y)
+  .orient("left").ticks(5);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.val); });
-
-var svg = d3.select("bcontent").append("svg")
+// Define the line
+var valueline = d3.svg.line()
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.close); });
+    
+// Adds the svg canvas 
+var chart1 = d3.select("comparecontent")
+  .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data-b.tsv", function(error, data) {
+
+                
+
+// Get the data ------------------------------- our data ----------------------------------
+
+
+
+d3.json("data/dataset-sample.json", function(error,data) {
+  dataset = data.data;
+  //console.log(data);
+             
+  
+  dataset.forEach(function(d) {
+    //console.log(d)
+    if (d.Territórios) {
+      console.log(d.Territórios);
+      
+    }
+  })
+  });
+  
+
+// Get the data
+d3.csv("data1.csv", function(error, data) {
   data.forEach(function(d) {
     d.date = parseDate(d.date);
-    d.val = +d.val;
+    d.close = +d.close;
   });
 
+  // Scale the range of the data
   x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.val; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  // Add the valueline path.
+  chart1.append("path")
+    .attr("class", "line")
+    .attr("d", valueline(data));
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Price B ($)");
+  // Add the X Axis
+  chart1.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
+  // Add the Y Axis
+  chart1.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
 
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
 });
 
 })(d3);
---
+++ js/dropdown.js
@@ -0,0 +1,62 @@
+!(function (d3) {
+
+
+function checkIt(data) {
+    data =data.data;
+    
+    //var regexp = new RegExp(',');
+    var countriesByName = d3.nest()
+        .key(function (d) {
+          
+        return d.Territórios; 
+    })
+        .entries(data);
+// creating dropdown    
+    var data = JSON.stringify(countriesByName)
+    var data = JSON.parse(data);
+	var dropDown = d3.select("#dropdown_container")
+                   .append("select")
+                   .attr("class", "selection")
+                    .attr("name", "country-list");
+
+    var options = dropDown.selectAll("option")
+                  .data(data)
+                  .enter()
+                  .append("option");
+         options.text(function (d) { return d.key; })
+       .attr("value", function (d) { return d.key; });
+
+// detecting change in drop down
+var changePie = function() {
+    //get the data value and index from the event
+    var selectedValue = d3.event.target.value;
+    var selectedIndex = d3.event.target.selectedIndex;
+    
+    //alert("You selected the option at index " + selectedIndex + ", with value attribute "+ selectedValue);
+    
+    var selectedDOMElement =
+        d3.event.target.children[selectedIndex];
+    var selection = d3.select(selectedDOMElement);
+
+// subsetting data    
+var uniqueData = d3.nest()
+    .key(function(selection) { return selection.key; })
+    .entries(data)
+    .map(function(entry) { return entry.values[0]; });
+
+//Output selected country with all its values
+    console.log("your selection is");
+    console.dir(uniqueData[selectedIndex].values[0]);
+    
+    //making Pie 
+function makePie() {
+return ("you have made a Pie for" + " " + selection.text())
+};
+    alert(makePie());
+};   
+
+d3.select("#dropdown_container").on("change", changePie);           
+};
+
+d3.json("data/dataset-sample.json", checkIt);
+})(d3);
