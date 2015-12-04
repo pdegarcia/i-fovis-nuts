25d768c77bf648808dff888c8d5e2084911fc384
Paulo Garcia <paulo.garcia@tecnico.ulisboa.pt>
2015-12-04 00:13:14 +0000

    Search box com autocomplete funca.

--- js/searchbox.js
+++ js/searchbox.js
@@ -1,59 +1,115 @@
-!(function (d3) {
-
-function checkIt(data) {
-    data = data.document.nuts;
-
-    var nutsByName = d3.nest()
-        .key(function (d) {
-        return d._id;
-    })
-        .entries(data);
-
-    var data = JSON.stringify(nutsByName)
-    var data = JSON.parse(data);
-
-  	var searchbox = d3.select("#searchbox_container")
-                      .append("select")
-                      .attr("class", "selection")
-                      .attr("name", "country-list");
-
-    var options = searchbox.selectAll("option")
-                  .data(data)
-                  .enter()
-                  .append("option");
-
-    options.text(function (d) { return d.key; })
-           .attr("value", function (d) { return d.key; });
-
-// detecting change in drop down
-var changePie = function() {
-    //get the data value and index from the event
-    var selectedValue = d3.event.target.value;
-    var selectedIndex = d3.event.target.selectedIndex;
-
-    var selectedDOMElement =
-        d3.event.target.children[selectedIndex];
-    var selection = d3.select(selectedDOMElement);
-
-// subsetting data
-var uniqueData = d3.nest()
-    .key(function(selection) { return selection.key; })
-    .entries(data)
-    .map(function(entry) { return entry.values[0]; });
-
-//Output selected country with all its values
-    console.log("your selection is");
-    console.dir(uniqueData[selectedIndex].values[0]);
-
-    //making Pie
-function makePie() {
-return ("you selected: " + " " + selection.text() + " - " + uniqueData[selectedIndex].values[0].ambito)
-};
-    alert(makePie());
+var names = {
+  nutsi: ["Continente"],
+  nutsii: ["Norte", "Centro", "Área Metropolitana de Lisboa", "Alentejo", "Algarve"],
+  nutsiii: ["Alto Minho", "Cávado", "Ave", "Área Metropolitana do Porto", "Alto Tâmega", "Tâmega e Sousa",
+    "Douro", "Terras de Trás-os-Montes","Oeste", "Região de Aveiro", "Região de Coimbra", "Região de Leiria",
+    "Viseu Dão Lafões", "Beira Baixa", "Médio Tejo", "Beiras e Serra da Estrela", "Área Metropolitana de Lisboa",
+    "Alentejo Litoral", "Baixo Alentejo", "Lezíria do Tejo", "Alto Alentejo", "Alentejo Central", "Algarve"],
+  municipios: ["Arcos de Valdevez", "Caminha", "Melgaço", "Monção", "Paredes de Coura", "Ponte da Barca", "Ponte de Lima",
+  "Valença", "Viana do Castelo", "Vila Nova de Cerveira", "Amares", "Barcelos","Braga","Esposende","Terras de Bouro",
+  "Vila Verde","Cabeceiras de Basto","Fafe","Guimarães","Mondim de Basto","Póvoa de Lanhoso","Vieira do Minho",
+  "Vila Nova de Famalicão","Vizela","Arouca","Espinho","Gondomar","Maia","Matosinhos","Oliveira de Azeméis","Paredes","Porto",
+  "Póvoa de Varzim","Santa Maria da Feira","Santo Tirso","São João da Madeira","Trofa","Vale de Cambra","Valongo","Vila do Conde",
+  "Vila Nova de Gaia","Boticas","Chaves","Montalegre","Ribeira de Pena","Valpaços","Vila Pouca de Aguiar","Amarante","Baião",
+  "Castelo de Paiva","Celorico de Basto","Cinfães","Felgueiras","Lousada","Marco de Canaveses","Paços de Ferreira","Penafiel",
+  "Resende","Alijó","Armamar","Carrazeda de Ansiães","Freixo de Espada à Cinta","Lamego","Mesão Frio","Moimenta da Beira","Murça",
+  "Penedono","Peso da Régua","Sabrosa","Santa Marta de Penaguião","São João da Pesqueira","Sernancelhe","Tabuaço", "Tarouca","Torre de Moncorvo",
+  "Vila Nova de Foz Côa","Vila Real","Alfândega da Fé","Bragança","Macedo de Cavaleiros","Miranda do Douro","Mirandela","Mogadouro","Vila Flor",
+  "Vimioso","Vinhais","Alcobaça","Alenquer","Arruda dos Vinhos","Bombarral","Cadaval","Caldas da Rainha","Lourinhã","Nazaré","Óbidos",
+  "Peniche","Sobral de Monte Agraço","Torres Vedras","Águeda","Albergaria-a-Velha","Anadia","Aveiro","Estarreja","Ílhavo","Murtosa",
+  "Oliveira do Bairro","Ovar","Sever do Vouga","Vagos","Arganil","Cantanhede","Coimbra","Condeixa-a-Nova","Figueira da Foz","Góis",
+  "Lousã","Mealhada","Mira","Miranda do Corvo","Montemor-o-Velho","Mortágua","Oliveira do Hospital","Pampilhosa da Serra","Penacova",
+  "Penela","Soure","Tábua","Vila Nova de Poiares","Alvaiázere","Ansião","Batalha","Castanheira de Pêra","Figueiró dos Vinhos",
+  "Leiria","Marinha Grande","Pedrógão Grande","Pombal","Porto de Mós","Aguiar da Beira","Carregal do Sal","Castro Daire","Mangualde",
+  "Nelas","Oliveira de Frades","Penalva do Castelo","Santa Comba Dão","São Pedro do Sul","Sátão","Tondela","Vila Nova de Paiva",
+  "Viseu","Vouzela","Castelo Branco","Idanha-a-Nova","Oleiros","Penamacor","Proença-a-Nova","Vila Velha de Ródão","Abrantes",
+  "Alcanena","Constância","Entroncamento","Ferreira do Zêzere","Mação","Ourém","Sardoal","Sertã","Tomar","Torres Novas","Vila de Rei",
+  "Vila Nova da Barquinha","Almeida","Belmonte","Celorico da Beira","Covilhã","Figueira de Castelo Rodrigo","Fornos de Algodres",
+  "Fundão","Gouveia","Guarda","Manteigas","Mêda","Pinhel","Sabugal","Seia","Trancoso","Alcochete","Almada","Amadora","Barreiro",
+  "Cascais","Lisboa","Loures","Mafra","Moita","Montijo","Odivelas","Oeiras","Palmela","Seixal","Sesimbra","Setúbal","Sintra",
+  "Vila Franca de Xira","Alcácer do Sal","Grândola","Odemira","Santiago do Cacém","Sines","Aljustrel","Almodôvar","Alvito",
+  "Barrancos","Beja","Castro Verde","Cuba","Ferreira do Alentejo","Mértola","Moura","Ourique","Serpa","Vidigueira","Almeirim",
+  "Alpiarça","Azambuja","Benavente","Cartaxo","Chamusca","Coruche","Golegã","Rio Maior","Salvaterra de Magos","Santarém",
+  "Alter do Chão","Arronches","Avis","Campo Maior","Castelo de Vide","Crato","Elvas","Fronteira","Gavião","Marvão","Monforte",
+  "Nisa","Ponte de Sor","Portalegre","Sousel","Alandroal","Arraiolos","Borba","Estremoz","Évora","Montemor-o-Novo","Mora",
+  "Mourão","Portel","Redondo","Reguengos de Monsaraz","Vendas Novas","Viana do Alentejo","Vila Viçosa","Albufeira","Alcoutim",
+  "Aljezur","Castro Marim","Faro","Lagoa","Lagos","Loulé","Monchique","Olhão","Portimão","São Brás de Alportel","Silves","Tavira",
+  "Vila do Bispo","Vila Real de Santo António"]
 };
 
-d3.select("#searchbox_container").on("change", changePie);           
-};
+$('#areas-query').typeahead({
+  minLength: 1,
+  order: "asc",
+  group: true,
+  maxItemPerGroup: 3,
 
-d3.json("data/dataset.json", checkIt);
-})(d3);
+  groupOrder: function(){
+
+       var scope = this,
+           sortGroup = [];
+
+       for (var i in this.result) {
+           sortGroup.push({
+               group: i,
+               length: this.result[i].length
+           });
+       }
+
+       sortGroup.sort(
+           scope.helper.sort(
+               ["length"],
+               false, // false = desc, the most results on top
+               function (a) {
+                   return a.toString().toUpperCase()
+               }
+           )
+       );
+
+       return $.map(sortGroup, function (val, i) {
+           return val.group
+       });
+   },
+
+   hint: true,
+   dropdownFilter: "All",
+   href: "{{display}}",   /* Container que pode ser formatado como quisermos. */
+   template: "{{display}}, <small><em>{{group}}</em></small>",
+   source: {
+       "NUT I": {
+           data: names.nutsi
+       },
+       "NUT II": {
+           data: names.nutsii
+       },
+       "NUT III": {
+           data: names.nutsiii
+       },
+       "Municípios": {
+           data: names.municipios
+       }
+   },
+   callback: {
+       onClickAfter: function (node, a, item, event) {
+
+           var r = confirm("You will be redirected to:\n" + item.href + "\n\nContinue?");
+           if (r == true) {
+               window.open(item.href);
+           }
+
+           $('#result-container').text('');
+
+       },
+       onResult: function (node, query, obj, objCount) {
+
+           console.log(objCount)
+
+           var text = "";
+           if (query !== "") {
+               text = objCount + ' elements matching "' + query + '"';
+           }
+           $('#result-container').text(text);
+
+       }
+   },
+   debug: true
+});
